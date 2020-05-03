d61d5cc13d7715f5a689319ad2e8ffa643ad522b
Amos <amosbi12@gmail.com>
2020-05-04 02:10:17 +0300

    Will complete StatisticsProps interface and add QueryRange interface

--- src/interfaces/StatisticsProps.interface.ts
+++ src/interfaces/StatisticsProps.interface.ts
@@ -1,1 +1,7 @@
-export default interface StatisticsProps {}
+import QueryRange from "./queryRange.interface";
+
+export default interface StatisticsProps {
+  dataSize: string;
+  numOfIndices: number;
+  queryRange: QueryRange;
+}
