16620ef1bca27f581e0e20a0aff4f0f4089e1c9b
Amos <amosbi12@gmail.com>
2020-05-04 13:04:27 +0300

    Will add three inputs and refrences

--- src/components/QueryControl.tsx
+++ src/components/QueryControl.tsx
@@ -3,6 +3,9 @@ import QueryControlState from "../interfaces/QueryControlState.interface";
 
 class QueryControl extends Component<{}, QueryControlState> {
   state: QueryControlState;
+  index: React.RefObject<HTMLInputElement>;
+  environ: React.RefObject<HTMLInputElement>;
+  query: React.RefObject<HTMLInputElement>;
 
   constructor(props: {}) {
     super(props);
@@ -12,10 +15,28 @@ class QueryControl extends Component<{}, QueryControlState> {
       rank: null,
       statistics: null,
     };
+    this.index = React.createRef();
+    this.environ = React.createRef();
+    this.query = React.createRef();
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <label>
+          Index:
+          <input type="text" ref={this.index} />
+        </label>
+        <label>
+          Environment:
+          <input type="text" ref={this.environ} />
+        </label>
+        <label>
+          Query:
+          <input type="text" ref={this.query} />
+        </label>
+      </div>
+    );
   }
 }
 
