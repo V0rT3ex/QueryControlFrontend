0c4f3752581679d214d67bbe96079a14a0a4b238
Amos <amosbi12@gmail.com>
2020-05-04 03:24:55 +0300

    Will complete Rank component and add fictional data to App.tsx about Rank

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Statistics from "./components/Statistics";
+import Rank from "./components/Rank";
 
 class App extends Component {
   render() {
@@ -9,6 +10,21 @@ class App extends Component {
           dataSize="10KB"
           numOfIndices={12}
           queryRange={{ years: 1, months: 4 }}
+        />
+        <Rank
+          queryScore={9}
+          conditions={[
+            {
+              name: "Size too big",
+              percent: 7,
+              details: "What is wrong?!",
+            },
+            {
+              name: "Too many aggergations",
+              percent: 712,
+              details: "Cannot you see?!",
+            },
+          ]}
         />
       </div>
     );
--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import RankProps from "../interfaces/RankProps.interface";
 
-class Rank extends Component {
+class Rank extends Component<RankProps> {
+  constructor(props: RankProps) {
+    super(props);
+  }
+
   render() {
-    return <div></div>;
+    const { queryScore, conditions } = this.props;
+    return (
+      <div>
+        <p>
+          Query score: {queryScore}
+          Conditions:{" "}
+          {conditions.map((condition) => (
+            <span>
+              <p>
+                name: {condition.name} percent: {condition.percent}
+                details: {condition.details}
+              </p>
+            </span>
+          ))}
+        </p>
+      </div>
+    );
   }
 }
 
