7356c9d363148ade0f58292095daa6014f3f2524
Amos <amosbi12@gmail.com>
2020-05-04 13:36:46 +0300

    Will install axios package and add submit function and button in QueryControl component

--- src/components/QueryControl.tsx
+++ src/components/QueryControl.tsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import axios from "axios";
 import QueryControlState from "../interfaces/QueryControlState.interface";
+import { stat } from "fs";
+
+const serverUrl = "http://localhost:8080/";
 
 class QueryControl extends Component<{}, QueryControlState> {
   state: QueryControlState;
@@ -20,6 +24,50 @@ class QueryControl extends Component<{}, QueryControlState> {
     this.query = React.createRef();
   }
 
+  submit = async () => {
+    const indexValue = this.index.current.value;
+    const environValue = this.environ.current.value;
+    const queryValue = this.query.current.value;
+    const requestdata = {
+      index: indexValue,
+      env: environValue,
+      query: queryValue,
+    };
+
+    try {
+      const statisticRequest = await axios.post(
+        serverUrl + "search_statistics",
+        requestdata
+      );
+      const queryRequest = await axios.post(
+        serverUrl + "query_score",
+        requestdata
+      );
+      const statisticsData = statisticRequest.data;
+      const queryData = queryRequest.data;
+
+      this.setState({
+        error: false,
+        statistics: {
+          dataSize: statisticsData["data size"],
+          numOfIndices: statisticsData["number of indices"],
+          queryRange: statisticsData["longest time range"],
+        },
+        rank: {
+          queryScore: queryData["score"],
+          conditions: queryData["conditions"],
+        },
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage:
+          "Could not make a request to the server. Please contact the maintaining team.",
+      });
+      return;
+    }
+  };
+
   render() {
     return (
       <div>
@@ -35,6 +83,7 @@ class QueryControl extends Component<{}, QueryControlState> {
           Query:
           <input type="text" ref={this.query} />
         </label>
+        <button onClick={this.submit}>Submit</button>
       </div>
     );
   }
