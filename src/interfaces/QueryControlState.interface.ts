17b36cd2f1c67532093646588580c5273dcd7720
Amos <amosbi12@gmail.com>
2020-05-04 12:51:14 +0300

    Will complete QueryControlState interface

--- src/interfaces/QueryControlState.interface.ts
+++ src/interfaces/QueryControlState.interface.ts
@@ -1,1 +1,9 @@
-export default interface QueryControlState {}
+import RankProps from "./RankProps.interface";
+import StatisticsProps from "./StatisticsProps.interface";
+
+export default interface QueryControlState {
+  error: boolean;
+  errorMessage: string;
+  rank: RankProps;
+  statistics: StatisticsProps;
+}
