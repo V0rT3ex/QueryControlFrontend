165bcfc574f266334c864863eea66eb6ad50391d
Amos <amosbi12@gmail.com>
2020-05-04 12:57:47 +0300

    Will create constructor and define state variable type

--- src/components/QueryControl.tsx
+++ src/components/QueryControl.tsx
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
+import QueryControlState from "../interfaces/QueryControlState.interface";
 
-class QueryControl extends Component {
+class QueryControl extends Component<{}, QueryControlState> {
+  state: QueryControlState;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      error: false,
+      errorMessage: "",
+      rank: null,
+      statistics: null,
+    };
+  }
+
   render() {
     return <div></div>;
   }
